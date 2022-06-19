f6e7e220a078d3adfa1ed002da238af97cbb162b
sergey-png <svsobolev@miem.hse.ru>
2022-06-20 00:19:57 +0300

    small tests for db

---
+++ tests/db/test_db.py
@@ -0,0 +1,26 @@
+import pytest
+
+import logging
+from db.init import create_session
+
+logging.getLogger("uvicorn.error")
+
+
+def test_create_session():
+    with create_session() as session:
+        assert session is not None
+
+
+def test_create_session_twice():
+    with create_session() as session:
+        assert session is not None
+    with create_session() as session:
+        assert session is not None
+
+
+def test_create_session_and_rise_exception():
+    with pytest.raises(Exception):
+        with create_session() as session:
+            assert session is not None
+            raise Exception("test")
+
